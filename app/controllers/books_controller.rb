f756eb8e811f75d1b53b3a23146511a0d3300e07
Tsutomu Kuroda <t-kuroda@oiax.jp>
2011-07-31 00:29:26 +0900

    第1巻第8章

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -6,4 +6,17 @@ class BooksController < ApplicationController
   def show
     @book = Book.find(params[:id])
   end
+
+  def new
+    @book = Book.new
+  end
+
+  def create
+    @book = Book.new(params[:book])
+    if @book.save
+      redirect_to @book
+    else
+      render :new
+    end
+  end
 end
