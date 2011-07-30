d04265a6697af0653af9c98e50485ddf5e6ecc0c
Tsutomu Kuroda <t-kuroda@oiax.jp>
2011-07-31 00:54:37 +0900

    第1巻第13章

--- app/helpers/books_helper.rb
+++ app/helpers/books_helper.rb
@@ -1,2 +1,15 @@
+# -*- encoding: UTF-8 -*-
+
 module BooksHelper
+  def links_for(book)
+    links = []
+    links << link_to("修正", [ :edit, book ])
+    if book.checked_out?
+      links << link_to("戻す", [ :check_in, book ], :method => :put)
+    else
+      links << link_to("貸し出し", [ :check_out, book ], :method => :put)
+    end
+    links << delete_link(book)
+    raw(links.join(" "))
+  end
 end
